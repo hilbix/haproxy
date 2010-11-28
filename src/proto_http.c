208d2687e61422c2112909147950068f3e034434
Willy Tarreau <w@1wt.eu>
2010-11-28 06:57:24 +0100

    [BUG] http: correctly update the header list when removing two consecutive headers
    When a header is removed, the previous header's next pointer is updated
    to reflect the next of the current header. However, when cycling through
    the loop, we update the prev pointer to point to the deleted header, which
    means that if we delete another header, it's the deleted header's next
    pointer that will be updated, leaving the deleted header in the list with
    a null length, which is forbidden.
    
    We must just not update the prev pointer after a removal.
    
    This bug was present when either "reqdel" and "rspdel" removed two consecutive
    headers. It could also occur when removing cookies in either requests or
    responses, but since headers were the last header processing, the issue
    remained unnoticed.
    
    Issue reported by Hank A. Paulson.
    
    This fix must be ported to 1.4 and possibly 1.3.
    (cherry picked from commit 26db59ea6b1f7319f53aca34bc64c4f180247e98)
    (cherry picked from commit 1337ca81b88fce9944ad5baaa816ab4b1be929a1)

--- src/proto_http.c
+++ src/proto_http.c
@@ -3225,6 +3225,7 @@ int apply_filter_to_req_headers(struct session *t, struct buffer *req, struct hd
 				txn->hdr_idx.used--;
 				cur_hdr->len = 0;
 				cur_end = NULL; /* null-term has been rewritten */
+				cur_idx = old_idx;
 				break;
 
 			}
@@ -3928,6 +3929,7 @@ void manage_client_side_cookies(struct session *t, struct buffer *req)
 				txn->hdr_idx.v[old_idx].next = cur_hdr->next;
 				txn->hdr_idx.used--;
 				cur_hdr->len = 0;
+				cur_idx = old_idx;
 			}
 			hdr_next += delta;
 			http_msg_move_end(&txn->req, delta);
@@ -4020,6 +4022,7 @@ int apply_filter_to_resp_headers(struct session *t, struct buffer *rtr, struct h
 				txn->hdr_idx.used--;
 				cur_hdr->len = 0;
 				cur_end = NULL; /* null-term has been rewritten */
+				cur_idx = old_idx;
 				break;
 
 			}
@@ -4388,6 +4391,7 @@ void manage_server_side_cookies(struct session *t, struct buffer *res)
 						txn->hdr_idx.v[old_idx].next = cur_hdr->next;
 						txn->hdr_idx.used--;
 						cur_hdr->len = 0;
+						cur_idx = old_idx;
 						hdr_next += delta;
 						http_msg_move_end(&txn->rsp, delta);
 						/* note: while both invalid now, <next> and <hdr_end>

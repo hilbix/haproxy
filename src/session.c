45bf5f421a0770b2fbe6334c1276d236bb8c17d9
Willy Tarreau <w@1wt.eu>
2011-04-06 18:50:11 +0200

    [BUG] session: release slot before processing pending connections
    When a connection error is encountered on a server and the server's
    connection pool is full, pending connections are not woken up because
    the current connection is still accounted for on the server, so it
    still appears full. This becomes visible on a server which has
    "maxconn 1" because the pending connections will only be able to
    expire in the queue.
    
    Now we take care of releasing our current connection before trying to
    offer it to another pending request, so that the server can accept a
    next connection.
    
    This patch should be backported to 1.4.
    (cherry picked from commit b89cfca49428e79af1514c782cf0654cda3938ba)
    (cherry picked from commit ced86ed5196468f48fba173c780810d1b86d9318)

--- src/session.c
+++ src/session.c
@@ -260,6 +260,7 @@ int sess_update_st_cer(struct session *s, struct stream_interface *si)
 		if (s->srv)
 			s->srv->failed_conns++;
 		s->be->failed_conns++;
+		sess_change_server(s, NULL);
 		if (may_dequeue_tasks(s->srv, s->be))
 			process_srv_queue(s->srv);
 
@@ -282,6 +283,7 @@ int sess_update_st_cer(struct session *s, struct stream_interface *si)
 	 */
 	if (s->srv && s->conn_retries == 0 &&
 	    s->be->options & PR_O_REDISP && !(s->flags & SN_FORCE_PRST)) {
+		sess_change_server(s, NULL);
 		if (may_dequeue_tasks(s->srv, s->be))
 			process_srv_queue(s->srv);
 
@@ -396,6 +398,7 @@ void sess_update_stream_int(struct session *s, struct stream_interface *si)
 			s->be->failed_conns++;
 
 			/* release other sessions waiting for this server */
+			sess_change_server(s, NULL);
 			if (may_dequeue_tasks(s->srv, s->be))
 				process_srv_queue(s->srv);
 
